36699eb8729f2fa6698a937fa2891ae8bd6d2d63
Patricia <patricianjuguna084@gmail.com>
2025-08-26 13:58:30 +0300

    Add click intro group for CLI in cli.py

--- cli.py
+++ cli.py
@@ -2,7 +2,18 @@
 import click
 from db import SessionLocal
 from models import Customer, Table, Reservation
-from tabulate import tabulate
 from datetime import datetime
+from rich.console import Console
+from rich.table import Table as RichTable
 
 session = SessionLocal()
+console = Console()
+
+# ----------CLI Welcome------------
+@click.group()
+def cli():
+    """🍋  Welcome to Little Lemon CLI  🍋"""
+    pass
+
+if __name__ == "__main__":
+    cli()
