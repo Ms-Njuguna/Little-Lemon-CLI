9f8ce5599bda6ee91bec33925a67a70024a2ecd7
Patricia <patricianjuguna084@gmail.com>
2025-08-26 12:03:50 +0300

    Update customer model with column data i.e customer attributes

--- models/customer.py
+++ models/customer.py
@@ -4,4 +4,14 @@ from sqlalchemy.orm import relationship
 from db import Base
 
 class Customer(Base):
-    pass
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email_address = Column(String, unique=True, nullable=False)
+    phone_number = Column(String, unique=True, nullable=False)
+
+    reservations = relationship("Reservation", back_populates="customer", cascade="all, delete-orphan")
+
+    
