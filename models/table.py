86bb43b2ec824c19f26f3e6278639614dd721594
Patricia <patricianjuguna084@gmail.com>
2025-08-26 12:14:07 +0300

    Update table model with column data i.e table attributes

--- models/table.py
+++ models/table.py
@@ -4,4 +4,10 @@ from sqlalchemy.orm import relationship
 from db import Base
 
 class Table(Base):
-    pass
+    __tablename__ = "tables"
+
+    id = Column(Integer, primary_key=True)
+    table_number = Column(Integer, unique=True, nullable=False)
+    capacity = Column(Integer, unique=True, nullable=False)
+
+    reservations = relationship("Reservation", back_populates="table", cascade="all, delete-orphan")
